8ae211c3faf1cd64f12c67699910303a52eb37e4
DESKTOP-V6ANAB8\user <lai225joseph@gmail.com>
2025-09-19 21:46:30 +0800

    XGBClassifier

--- LogisticRegressionForPriceClassification.py
+++ LogisticRegressionForPriceClassification.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-houseing_data = pd.read_csv('./housing.csv')
-houseing_data.sample(5)
-houseing_data = houseing_data.dropna()
-houseing_data.loc[houseing_data['median_house_value'] == 500001].count
-houseing_data = houseing_data.drop(houseing_data.loc[houseing_data['median_house_value'] == 500001].index) 
-houseing_data['ocean_proximity'].unique() 
-houseing_data = pd.get_dummies(houseing_data, columns=['ocean_proximity'])
+housing_data = pd.read_csv('./housing.csv')
+housing_data.sample(5)
+housing_data = housing_data.dropna()
+housing_data.loc[housing_data['median_house_value'] == 500001].count
+housing_data = housing_data.drop(housing_data.loc[housing_data['median_house_value'] == 500001].index) 
+housing_data['ocean_proximity'].unique() 
+housing_data = pd.get_dummies(housing_data, columns=['ocean_proximity'])
 
-median = houseing_data['median_house_value'].median()
+median = housing_data['median_house_value'].median()
 
-houseing_data['above_median'] = (houseing_data['median_house_value'] - median) > 0
+housing_data['above_median'] = (housing_data['median_house_value'] - median) > 0
 
-X = houseing_data.drop(['median_house_value', 'above_median'], axis=1)
-Y = houseing_data['above_median']
+X = housing_data.drop(['median_house_value', 'above_median'], axis=1)
+Y = housing_data['above_median']
 
 print(X.columns)
 
@@ -33,3 +33,26 @@ print(pd.DataFrame({'predicated': y_pred, 'actual': y_test}))
 
 from sklearn.metrics import accuracy_score
 print('Testing_score : ', accuracy_score(y_test, y_pred))
+
+# 新增代碼：使用XGBoost進行分類並比較
+from xgboost import XGBClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
+# 訓練XGBoost分類模型
+x_train.columns = [str(col).replace("[","").replace("]","").replace("<","").replace(">","") for col in x_train.columns]
+x_test.columns = [str(col).replace("[","").replace("]","").replace("<","").replace(">","") for col in x_test.columns]
+xgb_clf = XGBClassifier(random_state=42, use_label_encoder=False)
+xgb_clf.fit(x_train, y_train)
+y_pred_xgb = xgb_clf.predict(x_test)
+print('XGBoost Testing Score (Accuracy):', accuracy_score(y_test, y_pred_xgb))
+
+# 比較邏輯迴歸和XGBoost的性能
+print("\n--- Logistic Regression Classification Report ---")
+print(classification_report(y_test, y_pred))
+print("\n--- XGBoost Classification Report ---")
+print(classification_report(y_test, y_pred_xgb))
+
+# 比較特徵重要性
+plt.figure(figsize=(12, 5))
+plt.subplot(1, 2, 1)
+# 邏輯迴歸係數（取絕對值）
